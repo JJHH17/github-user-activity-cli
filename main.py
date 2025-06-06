4958279844c231b71bc72ca1da5a76b8a41804c5
James Hatfield <james.hatfield17@icloud.com>
2025-06-06 19:04:48 +0100

    Refactored main body of loop to ensure exit criteria can be met, added INT conversion in function argument for better exit criteria handling

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ import requests
 import json
 from api_handle import get_user_events
 
-# get_user_events(username)
-
 # Loop for program functionality
 flag = True
 
 # Used to gather user input
-while flag:
+while True:
     user_input = input("Please enter your GitHub username: ")
-    page_count = int(input("Please enter how many events you want to return: "))
-    if user_input == "Exit" or "exit" or page_count == "Exit" or "exit":
-        flag = False
 
-    get_user_events(user_input,page_count)
+    if user_input == "Exit" or user_input == "exit":
+        break
+
+    page_count = input("Please enter how many events you want to return: ")
+
+    if page_count == "Exit" or page_count == "exit":
+        break
+
+    get_user_events(user_input, int(page_count))
+
+# TODO: Add error handling for incorrect address, page numbers
+# TODO: Handle time and date formatting
+# TODO: Allow user to request specific event types
